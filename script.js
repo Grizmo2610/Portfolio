2bd1564322a7edf426c6d00caedeee85ce19b962
Grizmo2610 <hoangtuantu893@gmail.com>
2023-10-26 15:47:20 +0700

    birth day update

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ function copyTextToClipboard(elementId, successMessage) {
 
     alert(successMessage);
 }
+
+function checkNavigation(target) {
+    // Kiểm tra nếu URL hiện tại là trang gốc (index.html)
+    if (window.location.href.endsWith("/index.html")) {
+        // Không thực hiện điều hướng và hiển thị thông báo (hoặc thực hiện hành động khác)
+        alert("You are already on the home page.");
+        event.preventDefault(); // Ngăn chặn sự kiện điều hướng mặc định
+    }
+}
